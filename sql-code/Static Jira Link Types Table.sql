727bb931bf3a5b6fb095b5e6248f9fe5e97bdaf6
cd-ktreanor <cd.ktreanor@gmail.com>
2017-06-23 16:08:00 -0400

    Add files via upload

---
+++ sql-code/Static Jira Link Types Table.sql
@@ -0,0 +1,15 @@
+drop table JiraLinkTypes;
+create table JiraLinkTypes (Link_ID int, jira_subtask_link varchar(100), jira_subtask_inward varchar(100), jira_subtask_outward varchar(100), jira_subtask varchar(100));
+
+insert into JiraLinkTypes values (10010,'Existing Issue','Reported By','Exists In', NULL) ;
+insert into JiraLinkTypes values (10012,'Depends','Is Required By','Depends On', NULL) ;
+insert into JiraLinkTypes values (10020,'Duplicate','Is Duplicated By','Is a Duplicate of', NULL) ;
+insert into JiraLinkTypes values (10030,'Related','Related to','Relates to', NULL) ;
+insert into JiraLinkTypes values (10040,'Replace','Replaced By','Replaces', NULL) ;
+insert into JiraLinkTypes values (10050,'Finish-to-Start Dependency','Is Finish-to-Start Depended by','Finish-to-Start Depends on', NULL) ;
+insert into JiraLinkTypes values (10051,'Finish-to-Finish Dependency','Is Finish-to-Finish Depended by','Finish-to-Finish Depends on', NULL) ;
+insert into JiraLinkTypes values (10052,'Start-to-Finish Dependency','Is Start-to-Finish Depended by','Start-to-Finish Depends on', NULL) ;
+insert into JiraLinkTypes values (10053,'Start-to-Start Dependency','Is Start-to-Start Depended by','Start-to-Start Depends on', NULL) ;
+insert into JiraLinkTypes values (10150,'Epic-Story Link','Has Epic','Is Epic of','jira_gh_epic_story') ;
+insert into JiraLinkTypes values (10250,'Defect','Created By','Created', NULL) ;
+insert into JiraLinkTypes values (10350,'Tests','Tested By','Tests', NULL) ;
---
+++ sql-code/Static Project Number Table.sql
@@ -0,0 +1,28 @@
+drop table CDDates;
+create table CDProjectNumbers (ProjectNumber int,	ProjectName varchar(75));
+
+insert into CDProjectNumbers values (14362,'Matrix NRE Busware') ;
+insert into CDProjectNumbers values (14363,'Matrix NRE AVM') ;
+insert into CDProjectNumbers values (14364,'Matrix NRE Bustime') ;
+insert into CDProjectNumbers values (14365,'Matrix NRE BLFM') ;
+insert into CDProjectNumbers values (14366,'Matrix NRE CleverCAD') ;
+insert into CDProjectNumbers values (14367,'Matrix NRE CleverAnalytics') ;
+insert into CDProjectNumbers values (14368,'Matrix NRE Clever Reports') ;
+insert into CDProjectNumbers values (14369,'Matrix NRE CleverWorks') ;
+insert into CDProjectNumbers values (14370,'Matrix NRE FeatureTeam') ;
+insert into CDProjectNumbers values (14371,'Matrix NRE SmartYard') ;
+insert into CDProjectNumbers values (14372,'Matrix NRE Infotainment') ;
+insert into CDProjectNumbers values (14373,'Matrix NRE GreyHawk') ;
+insert into CDProjectNumbers values (15160,'Matrix NRE RCP') ;
+insert into CDProjectNumbers values (15060,'Matrix NRE CleverWare') ;
+insert into CDProjectNumbers values (10790,'Bustime Agile') ;
+insert into CDProjectNumbers values (11690,'Busware') ;
+insert into CDProjectNumbers values (11091,'CCADA') ;
+insert into CDProjectNumbers values (10693,'NRE') ;
+insert into CDProjectNumbers values (10390,'Cleverworks') ;
+insert into CDProjectNumbers values (10180,'Greyhawk') ;
+insert into CDProjectNumbers values (10590,'SmartYard') ;
+insert into CDProjectNumbers values (10200,'CRS') ;
+insert into CDProjectNumbers values (10291,'AVMA') ;
+insert into CDProjectNumbers values (10692,'ACE') ;
+insert into CDProjectNumbers values (12191,'CleverWare') ;
