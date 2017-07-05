8d35814b36fbb23c6fc407fdd2a6d0c9d22896e9
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 13:30:29 -0400

    Create GreyHawk_Input.sql

---
+++ sql-code/GreyHawk_Input.sql
@@ -0,0 +1,14 @@
+CREATE DEFINER=`readonly`@`%` PROCEDURE `GreyHawk_Input`()
+BEGIN
+	call GreyHawk_updateInput() ;
+    
+drop table if exists GreyHawk_InputTable ;
+create table GreyHawk_InputTable    
+    
+    Select GreyHawk_middleInput.* ,
+
+ifnull(case when GreyHawk_middleInput.Ticket_Closed = 1 then 0 else (case when (GreyHawk_middleInput.Closed_Story_Points + GreyHawk_middleInput.Remaining_Story_Points = 0) then GreyHawk_middleInput.Proposal_or_NRE_Hours else (case when (GreyHawk_middleInput.Closed_Story_Points + GreyHawk_middleInput.Remaining_Story_Points) > GreyHawk_middleInput.Proposal_or_NRE_Hours then GreyHawk_middleInput.Remaining_Story_Points else (GreyHawk_middleInput.Proposal_or_NRE_Hours - GreyHawk_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+
+from GreyHawk_middleInput ;
+
+END
