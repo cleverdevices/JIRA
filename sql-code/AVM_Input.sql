86bffb90ec7784a19a4babf3911bed8a9ab19314
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 09:43:26 -0400

    Create AVM_Input.sql

---
+++ sql-code/AVM_Input.sql
@@ -0,0 +1,14 @@
+CREATE DEFINER=`readonly`@`%` PROCEDURE `AVM_Input`()
+BEGIN
+	call AVM_updateInput() ;
+    
+drop table if exists AVM_InputTable ;
+create table AVM_InputTable    
+    
+    Select AVM_middleInput.* ,
+
+ifnull(case when AVM_middleInput.Ticket_Closed = 1 then 0 else (case when (AVM_middleInput.Closed_Story_Points + AVM_middleInput.Remaining_Story_Points = 0) then AVM_middleInput.Proposal_or_NRE_Hours else (case when (AVM_middleInput.Closed_Story_Points + AVM_middleInput.Remaining_Story_Points) > AVM_middleInput.Proposal_or_NRE_Hours then AVM_middleInput.Remaining_Story_Points else (AVM_middleInput.Proposal_or_NRE_Hours - AVM_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+
+from AVM_middleInput ;
+
+END
