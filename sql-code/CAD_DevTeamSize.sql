e3f90f6b1ff73928d63fe47ae702f6d0d094c18d
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 09:55:22 -0400

    Create CAD_DevTeamSize.sql

---
+++ sql-code/CAD_DevTeamSize.sql
@@ -0,0 +1,36 @@
+CREATE DEFINER=`readonly`@`%` PROCEDURE `CAD_DevTeamSize`()
+BEGIN
+declare num_days int;
+
+call CAD_Input();
+call CAD_tblVersions();
+
+drop table if exists CAD_DeveloperTeamSize;
+create table CAD_DeveloperTeamSize
+
+select  CAD_tblVersions.vname as Version , 43.5 as Team_Size, 
+
+(CAD_tblVersions.Column_0) as Total_NRE_per_Version,
+
+ CAD_InputTable.StartDate as Version_Start_Date, CAD_InputTable.ReleaseDate as Version_Release_Date,
+ 
+
+if(if(date(now()) > CAD_InputTable.StartDate, CDdateDiffFunction(date(now() - interval 7 day), CAD_InputTable.ReleaseDate)*0.7*8*21, CDdateDiffFunction(CAD_InputTable.StartDate, CAD_InputTable.ReleaseDate)*0.7*8*21) < 0, 0, if(date(now()) > CAD_InputTable.StartDate, CDdateDiffFunction(date(now() - interval 7 day), CAD_InputTable.ReleaseDate)*0.7*8*21, CDdateDiffFunction(CAD_InputTable.StartDate, CAD_InputTable.ReleaseDate)*0.7*8*21)) as Hours,
+
+if(if(date(now()) > CAD_InputTable.StartDate, CDdateDiffFunction(date(now() - interval 7 day), CAD_InputTable.ReleaseDate)*0.7*8*21, CDdateDiffFunction(CAD_InputTable.StartDate, CAD_InputTable.ReleaseDate)*0.7*8*21) < 0, 0, if(date(now()) > CAD_InputTable.StartDate, CDdateDiffFunction(date(now() - interval 7 day), CAD_InputTable.ReleaseDate)*0.7*8*21, CDdateDiffFunction(CAD_InputTable.StartDate, CAD_InputTable.ReleaseDate)*0.7*8*21)) - (CAD_tblVersions.Column_0) as Available_Hours
+
+from projectversion, CAD_tblVersions
+
+
+left join CAD_InputTable on CAD_InputTable.Version = CAD_tblVersions.vname where (projectversion.project in( 10291, 10692) and projectversion.released is null and projectversion.archived is null and projectversion.releasedate >0) 
+
+
+
+group by CAD_tblVersions.vname;
+
+select CAD_DeveloperTeamSize.*, workMatch.releaseRow as Release_Date_Includes_QA_Testing_and_Regression from CAD_DeveloperTeamSize
+left join workMatch on CAD_DeveloperTeamSize.Version_Release_Date = workMatch.calendar_date
+group by CAD_DeveloperTeamSize.Version;
+
+
+END
