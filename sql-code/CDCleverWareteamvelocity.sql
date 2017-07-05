4887d22e5f0f4c2ed836a9d7d7af8834245754df
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 11:24:17 -0400

    Create CDCleverWareteamvelocity.sql

---
+++ sql-code/CDCleverWareteamvelocity.sql
@@ -0,0 +1,10 @@
+drop table if exists CDCleverWareteamvelocity;
+create table CDCleverWareteamvelocity(Team varchar(25), Points int, storyHours float);
+
+insert into CDCleverWareteamvelocity values ('1', 1, 4) ;
+insert into CDCleverWareteamvelocity values ('1', 2, 8) ;
+insert into CDCleverWareteamvelocity values ('1', 3, 16) ;
+insert into CDCleverWareteamvelocity values ('1', 5, 24) ;
+insert into CDCleverWareteamvelocity values ('1', 8, 40) ;
+insert into CDCleverWareteamvelocity values ('1', 13, 56) ;
+insert into CDCleverWareteamvelocity values ('1', 21, 80) ;
