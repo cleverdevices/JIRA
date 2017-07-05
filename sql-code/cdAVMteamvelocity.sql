1be83f52b9b84f10bd7a27f7bf09ef32eaa042c1
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 14:57:05 -0400

    Create cdAVMteamvelocity.sql

---
+++ sql-code/cdAVMteamvelocity.sql
@@ -0,0 +1,13 @@
+drop table if exists cdAVMteamvelocity;
+create table cdAVMteamvelocity(Team varchar(25), Points int, storyHours float);
+
+insert into cdAVMteamvelocity values ('1', 1, 5.25) ;
+insert into cdAVMteamvelocity values ('1', 2, 10.5) ;
+insert into cdAVMteamvelocity values ('1', 3, 15.75) ;
+insert into cdAVMteamvelocity values ('1', 5, 26.25) ;
+insert into cdAVMteamvelocity values ('1', 8, 42) ;
+insert into cdAVMteamvelocity values ('1', 13, 68.25) ;
+insert into cdAVMteamvelocity values ('1', 21, 110.25) ;
+insert into cdAVMteamvelocity values ('1', 34, 178.5) ;
+insert into cdAVMteamvelocity values ('1', 55, 288.75) ;
+insert into cdAVMteamvelocity values ('1', 89, 467.25) ;
