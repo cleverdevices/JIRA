d618eb341837e8e29115f1e52eb93c17f476a89f
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 09:53:45 -0400

    Create Busware_Input.sql

---
+++ sql-code/Busware_Input.sql
@@ -0,0 +1,14 @@
+CREATE DEFINER=`readonly`@`%` PROCEDURE `Busware_Input`()
+BEGIN
+	call Busware_updateInput() ;
+    
+drop table if exists Busware_InputTable ;
+create table Busware_InputTable    
+    
+    Select Busware_middleInput.* ,
+
+ifnull(case when Busware_middleInput.Ticket_Closed = 1 then 0 else (case when (Busware_middleInput.Closed_Story_Points + Busware_middleInput.Remaining_Story_Points = 0) then Busware_middleInput.Proposal_or_NRE_Hours else (case when (Busware_middleInput.Closed_Story_Points + Busware_middleInput.Remaining_Story_Points) > Busware_middleInput.Proposal_or_NRE_Hours then Busware_middleInput.Remaining_Story_Points else (Busware_middleInput.Proposal_or_NRE_Hours - Busware_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+
+from Busware_middleInput ;
+
+END
