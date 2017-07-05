edfda3a7d043bbef0bf61fada8274f2c1a869607
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 13:03:24 -0400

    Create CDGreyHawkteamvelocity.sql

---
+++ sql-code/CDGreyHawkteamvelocity.sql
@@ -0,0 +1,9 @@
+drop table if exists CDGreyHawkteamvelocity;
+create table CDGreyHawkteamvelocity(Team varchar(25), Points int, storyHours float);
+
+insert into CDGreyHawkteamvelocity values ('1', 1, 6.5) ;
+insert into CDGreyHawkteamvelocity values ('1', 3, 19.5) ;
+insert into CDGreyHawkteamvelocity values ('1', 4, 26) ;
+insert into CDGreyHawkteamvelocity values ('1', 5, 32.5) ;
+insert into CDGreyHawkteamvelocity values ('1', 8, 52) ;
+insert into CDGreyHawkteamvelocity values ('1', 13, 84.5) ;
