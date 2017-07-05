4ed866fbff147fcbb53df929aa1fe97d3de1e159
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 14:38:18 -0400

    Create cdSmartYardteamvelocity.sql

---
+++ sql-code/cdSmartYardteamvelocity.sql
@@ -0,0 +1,8 @@
+drop table if exists cdSmartYardteamvelocity;
+create table cdSmartYardteamvelocity(Team varchar(25), Points int, storyHours float);
+
+insert into cdSmartYardteamvelocity values ('1', 1, 8) ;
+insert into cdSmartYardteamvelocity values ('1', 2, 40) ;
+insert into cdSmartYardteamvelocity values ('1', 5, 80) ;
+insert into cdSmartYardteamvelocity values ('1', 8, 80) ;
+
