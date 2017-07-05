6cd265791d1f911f920d0ac0d82c0d9f532b05a9
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-05 09:55:44 -0400

    Create CAD_Input.sql

---
+++ sql-code/CAD_Input.sql
@@ -0,0 +1,14 @@
+CREATE DEFINER=`readonly`@`%` PROCEDURE `CAD_Input`()
+BEGIN
+	call CAD_updateInput() ;
+    
+drop table if exists CAD_InputTable ;
+create table CAD_InputTable    
+    
+    Select CAD_middleInput.* ,
+
+ifnull(case when CAD_middleInput.Ticket_Closed = 1 then 0 else (case when (CAD_middleInput.Closed_Story_Points + CAD_middleInput.Remaining_Story_Points = 0) then CAD_middleInput.Proposal_or_NRE_Hours else (case when (CAD_middleInput.Closed_Story_Points + CAD_middleInput.Remaining_Story_Points) > CAD_middleInput.Proposal_or_NRE_Hours then CAD_middleInput.Remaining_Story_Points else (CAD_middleInput.Proposal_or_NRE_Hours - CAD_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+
+from CAD_middleInput ;
+
+END
