00856a612fb5bf6b0dd5a7cb25290aa3e519dd99
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-07 14:14:04 -0400

    Update Busware_tblVersions.sql

--- sql-code/Busware_tblVersions.sql
+++ sql-code/Busware_tblVersions.sql
@@ -5,8 +5,8 @@ drop table if exists Busware_tblVersions;
 create table Busware_tblVersions
 
 select projectversion.* ,
-case when (projectversion.vname = buswareinput.Version) then sum(buswareinput.Hours_to_Use) else 0 end as Column_0
+case when (projectversion.vname = Busware_InputTable.Version) then sum(Busware_InputTable.Hours_to_Use) else 0 end as Column_0
  
-from projectversion left join buswareinput on buswareinput.Version = projectversion.vname where (projectversion.project in( 11690, 10027) and projectversion.released is null and projectversion.archived is null and projectversion.releasedate >0) group by projectversion.vname;
+from projectversion left join Busware_InputTable on Busware_InputTable.Version = projectversion.vname where (projectversion.project in( 11690, 10027) and projectversion.released is null and projectversion.archived is null and projectversion.releasedate >0) group by projectversion.vname;
 
 END
