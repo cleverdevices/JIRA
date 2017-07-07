fe8aa0a925164c857762692bc0f580bfd35cff24
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-07 14:11:33 -0400

    Update AVM_DevTeamSize.sql

--- sql-code/AVM_DevTeamSize.sql
+++ sql-code/AVM_DevTeamSize.sql
@@ -2,8 +2,20 @@ CREATE DEFINER=`readonly`@`%` PROCEDURE `AVM_DevTeamSize`()
 BEGIN
 declare num_days int;
 
+call WorkDays();
 call AVM_Input();
 call AVM_tblVersions();
+
+call AVM_EpicUpdate();
+
+#Create Table from new query and join values from static table
+DROP TABLE IF EXISTS AVM_EpicUpdateLink;
+Create Table AVM_EpicUpdateLink 
+
+
+Select AVM_EpicUpdateTable.*, CDAVMteamvelocity.storyHours, case when issue_status = 1 then 1 else 0 end as column1  from AVM_EpicUpdateTable
+ join CDAVMteamvelocity 
+on AVM_EpicUpdateTable.Team = CDAVMteamvelocity.team and AVM_EpicUpdateTable.`Story Points` = CDAVMteamvelocity.points ;
 
 drop table if exists AVM_DeveloperTeamSize;
 create table AVM_DeveloperTeamSize
