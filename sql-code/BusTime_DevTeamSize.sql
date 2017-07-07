60f58377d688723c88c377885512fdd9e9635234
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-07 14:12:30 -0400

    Update BusTime_DevTeamSize.sql

--- sql-code/BusTime_DevTeamSize.sql
+++ sql-code/BusTime_DevTeamSize.sql
@@ -2,8 +2,20 @@ CREATE DEFINER=`readonly`@`%` PROCEDURE `BusTime_DevTeamSize`()
 BEGIN
 declare num_days int;
 
+call WorkDays();
 call BusTime_Input();
 call BusTime_tblVersions();
+call BusTime_EpicUpdate();
+
+#Create Table from new query and join values from static table
+DROP TABLE IF EXISTS BusTime_EpicUpdateLink;
+Create Table BusTime_EpicUpdateLink 
+
+
+Select BusTime_EpicUpdateTable.*, CDBusTimeteamvelocity.storyHours, case when issue_status = 1 then 1 else 0 end as column1  from BusTime_EpicUpdateTable
+ join CDBusTimeteamvelocity 
+on BusTime_EpicUpdateTable.Team = CDBusTimeteamvelocity.team and BusTime_EpicUpdateTable.`Story Points` = CDBusTimeteamvelocity.points ;
+
 
 drop table if exists BusTime_DeveloperTeamSize;
 create table BusTime_DeveloperTeamSize
