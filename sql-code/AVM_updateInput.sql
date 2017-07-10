2e9fdece1330b14dc94d55f26ba8e253d1ed5d8d
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-10 13:42:49 -0400

    Update AVM_updateInput.sql

--- sql-code/AVM_updateInput.sql
+++ sql-code/AVM_updateInput.sql
@@ -6,8 +6,9 @@ call AVM_TicketToUse() ;
 drop table if exists AVM_middleInput ;
 create table AVM_middleInput
 
-Select AVM_startInput.* ,case when ((AVM_EpicUpdateLink.epic_issuenum = AVM_startInput.Ticket_to_Use) AND AVM_EpicUpdateLink.column1 = 0) then round(sum(AVM_EpicUpdateLink.storyHours),0) else 0 end as Closed_Story_Points,
-case when ((AVM_EpicUpdateLink.epic_issuenum = AVM_startInput.Ticket_to_Use) AND AVM_EpicUpdateLink.column1 = 1) then round(sum(AVM_EpicUpdateLink.storyHours),0) else 0 end as Remaining_Story_Points
+Select AVM_startInput.* ,
+case when (AVM_EpicUpdateLink.epic_issuenum = AVM_startInput.Ticket_to_Use) then (select round(sum(AVM_EpicUpdateLink.storyHours)) from AVM_EpicUpdateLink where AVM_EpicUpdateLink.column1 = 0 and AVM_EpicUpdateLink.epic_issuenum = AVM_startInput.Ticket_to_Use ) else 0 end as Closed_Story_Points,
+case when (AVM_EpicUpdateLink.epic_issuenum = AVM_startInput.Ticket_to_Use) then (select round(sum(AVM_EpicUpdateLink.storyHours)) from AVM_EpicUpdateLink where AVM_EpicUpdateLink.column1 = 1 and AVM_EpicUpdateLink.epic_issuenum = AVM_startInput.Ticket_to_Use) else 0 end as Remaining_Story_Points
 from AVM_startInput left join AVM_EpicUpdateLink on AVM_EpicUpdateLink.epic_issuenum = AVM_startInput.Ticket_to_Use
 
 group by Manual_JIRA, NRE_JIRA, Linked, customer, version ;
