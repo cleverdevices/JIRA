60feb9827a11a3ab72883295e0d05d60213ec1e5
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-10 13:46:58 -0400

    Update Busware_updateInput.sql

--- sql-code/Busware_updateInput.sql
+++ sql-code/Busware_updateInput.sql
@@ -6,8 +6,9 @@ call Busware_TicketToUse() ;
 drop table if exists Busware_middleInput ;
 create table Busware_middleInput
 
-Select Busware_startInput.* ,case when ((Busware_EpicUpdateLink.epic_issuenum = Busware_startInput.Ticket_to_Use) AND Busware_EpicUpdateLink.column1 = 0) then round(sum(Busware_EpicUpdateLink.storyHours),0) else 0 end as Closed_Story_Points,
-case when ((Busware_EpicUpdateLink.epic_issuenum = Busware_startInput.Ticket_to_Use) AND Busware_EpicUpdateLink.column1 = 1) then round(sum(Busware_EpicUpdateLink.storyHours),0) else 0 end as Remaining_Story_Points
+Select Busware_startInput.* ,
+case when (Busware_EpicUpdateLink.epic_issuenum = Busware_startInput.Ticket_to_Use) then (select round(sum(Busware_EpicUpdateLink.storyHours)) from Busware_EpicUpdateLink where Busware_EpicUpdateLink.column1 = 0 and Busware_EpicUpdateLink.epic_issuenum = Busware_startInput.Ticket_to_Use ) else 0 end as Closed_Story_Points,
+case when (Busware_EpicUpdateLink.epic_issuenum = Busware_startInput.Ticket_to_Use) then (select round(sum(Busware_EpicUpdateLink.storyHours)) from Busware_EpicUpdateLink where Busware_EpicUpdateLink.column1 = 1 and Busware_EpicUpdateLink.epic_issuenum = Busware_startInput.Ticket_to_Use) else 0 end as Remaining_Story_Points
 from Busware_startInput left join Busware_EpicUpdateLink on Busware_EpicUpdateLink.epic_issuenum = Busware_startInput.Ticket_to_Use
 
 group by Manual_JIRA, NRE_JIRA, Linked, customer, version ;
