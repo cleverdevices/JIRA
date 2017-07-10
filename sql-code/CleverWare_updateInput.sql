4ddfa6a074f0148b6a161c1d443acce5dbf2c197
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-10 13:47:45 -0400

    Update CleverWare_updateInput.sql

--- sql-code/CleverWare_updateInput.sql
+++ sql-code/CleverWare_updateInput.sql
@@ -6,8 +6,9 @@ call CleverWare_TicketToUse() ;
 drop table if exists CleverWare_middleInput ;
 create table CleverWare_middleInput
 
-Select CleverWare_startInput.* ,case when ((CleverWare_EpicUpdateLink.epic_issuenum = CleverWare_startInput.Ticket_to_Use) AND CleverWare_EpicUpdateLink.column1 = 0) then round(sum(CleverWare_EpicUpdateLink.storyHours),0) else 0 end as Closed_Story_Points,
-case when ((CleverWare_EpicUpdateLink.epic_issuenum = CleverWare_startInput.Ticket_to_Use) AND CleverWare_EpicUpdateLink.column1 = 1) then round(sum(CleverWare_EpicUpdateLink.storyHours),0) else 0 end as Remaining_Story_Points
+Select CleverWare_startInput.* ,
+case when (Cleverware_EpicUpdateLink.epic_issuenum = Cleverware_startInput.Ticket_to_Use) then (select round(sum(Cleverware_EpicUpdateLink.storyHours)) from Cleverware_EpicUpdateLink where Cleverware_EpicUpdateLink.column1 = 0 and Cleverware_EpicUpdateLink.epic_issuenum = Cleverware_startInput.Ticket_to_Use ) else 0 end as Closed_Story_Points,
+case when (Cleverware_EpicUpdateLink.epic_issuenum = Cleverware_startInput.Ticket_to_Use) then (select round(sum(Cleverware_EpicUpdateLink.storyHours)) from Cleverware_EpicUpdateLink where Cleverware_EpicUpdateLink.column1 = 1 and Cleverware_EpicUpdateLink.epic_issuenum = Cleverware_startInput.Ticket_to_Use) else 0 end as Remaining_Story_Points
 from CleverWare_startInput left join CleverWare_EpicUpdateLink on CleverWare_EpicUpdateLink.epic_issuenum = CleverWare_startInput.Ticket_to_Use
 
 group by Manual_JIRA, NRE_JIRA, Linked, customer, version ;
