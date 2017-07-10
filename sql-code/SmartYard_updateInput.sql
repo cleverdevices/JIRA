402b8b02435d7bca5847e9d395abfd05299608ec
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-10 13:48:12 -0400

    Update SmartYard_updateInput.sql

--- sql-code/SmartYard_updateInput.sql
+++ sql-code/SmartYard_updateInput.sql
@@ -6,8 +6,9 @@ call SmartYard_TicketToUse() ;
 drop table if exists SmartYard_middleInput ;
 create table SmartYard_middleInput
 
-Select SmartYard_startInput.* ,case when ((SmartYard_EpicUpdateLink.epic_issuenum = SmartYard_startInput.Ticket_to_Use) AND SmartYard_EpicUpdateLink.column1 = 0) then round(sum(SmartYard_EpicUpdateLink.storyHours),0) else 0 end as Closed_Story_Points,
-case when ((SmartYard_EpicUpdateLink.epic_issuenum = SmartYard_startInput.Ticket_to_Use) AND SmartYard_EpicUpdateLink.column1 = 1) then round(sum(SmartYard_EpicUpdateLink.storyHours),0) else 0 end as Remaining_Story_Points
+Select SmartYard_startInput.* ,
+case when (SmartYard_EpicUpdateLink.epic_issuenum = SmartYard_startInput.Ticket_to_Use) then (select round(sum(SmartYard_EpicUpdateLink.storyHours)) from SmartYard_EpicUpdateLink where SmartYard_EpicUpdateLink.column1 = 0 and SmartYard_EpicUpdateLink.epic_issuenum = SmartYard_startInput.Ticket_to_Use ) else 0 end as Closed_Story_Points,
+case when (SmartYard_EpicUpdateLink.epic_issuenum = SmartYard_startInput.Ticket_to_Use) then (select round(sum(SmartYard_EpicUpdateLink.storyHours)) from SmartYard_EpicUpdateLink where SmartYard_EpicUpdateLink.column1 = 1 and SmartYard_EpicUpdateLink.epic_issuenum = SmartYard_startInput.Ticket_to_Use) else 0 end as Remaining_Story_Points
 from SmartYard_startInput left join SmartYard_EpicUpdateLink on SmartYard_EpicUpdateLink.epic_issuenum = SmartYard_startInput.Ticket_to_Use
 
 group by Manual_JIRA, NRE_JIRA, Linked, customer, version ;
