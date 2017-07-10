61815f5f07076b5bbc31b78071046e00b81e3fd9
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-10 13:48:01 -0400

    Update GreyHawk_updateInput.sql

--- sql-code/GreyHawk_updateInput.sql
+++ sql-code/GreyHawk_updateInput.sql
@@ -6,8 +6,9 @@ call GreyHawk_TicketToUse() ;
 drop table if exists GreyHawk_middleInput ;
 create table GreyHawk_middleInput
 
-Select GreyHawk_startInput.* ,case when ((GreyHawk_EpicUpdateLink.epic_issuenum = GreyHawk_startInput.Ticket_to_Use) AND GreyHawk_EpicUpdateLink.column1 = 0) then round(sum(GreyHawk_EpicUpdateLink.storyHours),0) else 0 end as Closed_Story_Points,
-case when ((GreyHawk_EpicUpdateLink.epic_issuenum = GreyHawk_startInput.Ticket_to_Use) AND GreyHawk_EpicUpdateLink.column1 = 1) then round(sum(GreyHawk_EpicUpdateLink.storyHours),0) else 0 end as Remaining_Story_Points
+Select GreyHawk_startInput.* ,
+case when (GreyHawk_EpicUpdateLink.epic_issuenum = GreyHawk_startInput.Ticket_to_Use) then (select round(sum(GreyHawk_EpicUpdateLink.storyHours)) from GreyHawk_EpicUpdateLink where GreyHawk_EpicUpdateLink.column1 = 0 and GreyHawk_EpicUpdateLink.epic_issuenum = GreyHawk_startInput.Ticket_to_Use ) else 0 end as Closed_Story_Points,
+case when (GreyHawk_EpicUpdateLink.epic_issuenum = GreyHawk_startInput.Ticket_to_Use) then (select round(sum(GreyHawk_EpicUpdateLink.storyHours)) from GreyHawk_EpicUpdateLink where GreyHawk_EpicUpdateLink.column1 = 1 and GreyHawk_EpicUpdateLink.epic_issuenum = GreyHawk_startInput.Ticket_to_Use) else 0 end as Remaining_Story_Points
 from GreyHawk_startInput left join GreyHawk_EpicUpdateLink on GreyHawk_EpicUpdateLink.epic_issuenum = GreyHawk_startInput.Ticket_to_Use
 
 group by Manual_JIRA, NRE_JIRA, Linked, customer, version ;
