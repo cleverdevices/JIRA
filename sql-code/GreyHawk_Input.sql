4297093624563a4feb72b2a8638f84eb2460848e
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-13 10:59:48 -0400

    Update GreyHawk_Input.sql

--- sql-code/GreyHawk_Input.sql
+++ sql-code/GreyHawk_Input.sql
@@ -7,11 +7,11 @@ create table GreyHawk_InputTable
     
     Select GreyHawk_middleInput.* ,
 
-ifnull(case when GreyHawk_middleInput.Ticket_Closed = 1 then 0 else (case when (GreyHawk_middleInput.Closed_Story_Points + GreyHawk_middleInput.Remaining_Story_Points = 0) then GreyHawk_middleInput.Proposal_or_NRE_Hours else (case when (GreyHawk_middleInput.Closed_Story_Points + GreyHawk_middleInput.Remaining_Story_Points) > GreyHawk_middleInput.Proposal_or_NRE_Hours then GreyHawk_middleInput.Remaining_Story_Points else (GreyHawk_middleInput.Proposal_or_NRE_Hours - GreyHawk_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+ifnull(case when GreyHawk_middleInput.Ticket_Closed = 1 then 0 else (case when (GreyHawk_middleInput.Closed_Story_Points + GreyHawk_middleInput.Remaining_Story_Points = 0) then GreyHawk_middleInput.Proposal_or_NRE_Hours else (case when (GreyHawk_middleInput.Closed_Story_Points + GreyHawk_middleInput.Remaining_Story_Points) > GreyHawk_middleInput.Proposal_or_NRE_Hours then GreyHawk_middleInput.Remaining_Story_Points else (GreyHawk_middleInput.Proposal_or_NRE_Hours - GreyHawk_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use,
+workMatch.releaseRow as Release_Actual
 
-from GreyHawk_middleInput ;
 
-select GreyHawk_InputTable.*, workMatch.releaseRow as Release_Actual from GreyHawk_InputTable
-left join workMatch on GreyHawk_InputTable.ReleaseDate = workMatch.calendar_date;
+from GreyHawk_middleInput 
+left join workMatch on GreyHawk_middleInput.ReleaseDate = workMatch.calendar_date;
 
 END
