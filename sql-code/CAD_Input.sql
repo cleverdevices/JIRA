cfffb9df8506c7e6e00e1fa910bf9e7f0283471a
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-13 10:59:01 -0400

    Update CAD_Input.sql

--- sql-code/CAD_Input.sql
+++ sql-code/CAD_Input.sql
@@ -7,11 +7,11 @@ create table CAD_InputTable
     
     Select CAD_middleInput.* ,
 
-ifnull(case when CAD_middleInput.Ticket_Closed = 1 then 0 else (case when (CAD_middleInput.Closed_Story_Points + CAD_middleInput.Remaining_Story_Points = 0) then CAD_middleInput.Proposal_or_NRE_Hours else (case when (CAD_middleInput.Closed_Story_Points + CAD_middleInput.Remaining_Story_Points) > CAD_middleInput.Proposal_or_NRE_Hours then CAD_middleInput.Remaining_Story_Points else (CAD_middleInput.Proposal_or_NRE_Hours - CAD_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+ifnull(case when CAD_middleInput.Ticket_Closed = 1 then 0 else (case when (CAD_middleInput.Closed_Story_Points + CAD_middleInput.Remaining_Story_Points = 0) then CAD_middleInput.Proposal_or_NRE_Hours else (case when (CAD_middleInput.Closed_Story_Points + CAD_middleInput.Remaining_Story_Points) > CAD_middleInput.Proposal_or_NRE_Hours then CAD_middleInput.Remaining_Story_Points else (CAD_middleInput.Proposal_or_NRE_Hours - CAD_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use,
+workMatch.releaseRow as Release_Actual
 
-from CAD_middleInput ;
 
-select CAD_InputTable.*, workMatch.releaseRow as Release_Actual from CAD_InputTable
-left join workMatch on CAD_InputTable.ReleaseDate = workMatch.calendar_date;
+from CAD_middleInput 
+left join workMatch on CAD_middleInput.ReleaseDate = workMatch.calendar_date;
 
 END
