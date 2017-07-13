f2c19152f8bda8f9aea826ba04f7cc0afe93c64e
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-13 10:58:49 -0400

    Update Busware_Input.sql

--- sql-code/Busware_Input.sql
+++ sql-code/Busware_Input.sql
@@ -7,11 +7,11 @@ create table Busware_InputTable
     
     Select Busware_middleInput.* ,
 
-ifnull(case when Busware_middleInput.Ticket_Closed = 1 then 0 else (case when (Busware_middleInput.Closed_Story_Points + Busware_middleInput.Remaining_Story_Points = 0) then Busware_middleInput.Proposal_or_NRE_Hours else (case when (Busware_middleInput.Closed_Story_Points + Busware_middleInput.Remaining_Story_Points) > Busware_middleInput.Proposal_or_NRE_Hours then Busware_middleInput.Remaining_Story_Points else (Busware_middleInput.Proposal_or_NRE_Hours - Busware_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+ifnull(case when Busware_middleInput.Ticket_Closed = 1 then 0 else (case when (Busware_middleInput.Closed_Story_Points + Busware_middleInput.Remaining_Story_Points = 0) then Busware_middleInput.Proposal_or_NRE_Hours else (case when (Busware_middleInput.Closed_Story_Points + Busware_middleInput.Remaining_Story_Points) > Busware_middleInput.Proposal_or_NRE_Hours then Busware_middleInput.Remaining_Story_Points else (Busware_middleInput.Proposal_or_NRE_Hours - Busware_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use,
+workMatch.releaseRow as Release_Actual
 
-from Busware_middleInput ;
 
-select Busware_InputTable.*, workMatch.releaseRow as Release_Actual from Busware_InputTable
-left join workMatch on Busware_InputTable.ReleaseDate = workMatch.calendar_date;
+from Busware_middleInput 
+left join workMatch on Busware_middleInput.ReleaseDate = workMatch.calendar_date;
 
 END
