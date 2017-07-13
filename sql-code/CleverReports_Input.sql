5edadb550868df0769d8f08fc400c72fb9eaedbb
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-13 10:59:14 -0400

    Update CleverReports_Input.sql

--- sql-code/CleverReports_Input.sql
+++ sql-code/CleverReports_Input.sql
@@ -7,11 +7,10 @@ create table CleverReports_InputTable
     
     Select CleverReports_middleInput.* ,
 
-ifnull(case when CleverReports_middleInput.Ticket_Closed = 1 then 0 else (case when (CleverReports_middleInput.Closed_Story_Points + CleverReports_middleInput.Remaining_Story_Points = 0) then CleverReports_middleInput.Proposal_or_NRE_Hours else (case when (CleverReports_middleInput.Closed_Story_Points + CleverReports_middleInput.Remaining_Story_Points) > CleverReports_middleInput.Proposal_or_NRE_Hours then CleverReports_middleInput.Remaining_Story_Points else (CleverReports_middleInput.Proposal_or_NRE_Hours - CleverReports_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+ifnull(case when CleverReports_middleInput.Ticket_Closed = 1 then 0 else (case when (CleverReports_middleInput.Closed_Story_Points + CleverReports_middleInput.Remaining_Story_Points = 0) then CleverReports_middleInput.Proposal_or_NRE_Hours else (case when (CleverReports_middleInput.Closed_Story_Points + CleverReports_middleInput.Remaining_Story_Points) > CleverReports_middleInput.Proposal_or_NRE_Hours then CleverReports_middleInput.Remaining_Story_Points else (CleverReports_middleInput.Proposal_or_NRE_Hours - CleverReports_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use,
+workMatch.releaseRow as Release_Actual
 
-from CleverReports_middleInput ;
 
-select CleverReports_InputTable.*, workMatch.releaseRow as Release_Actual from CleverReports_InputTable
-left join workMatch on CleverReports_InputTable.ReleaseDate = workMatch.calendar_date;
-
+from CleverReports_middleInput 
+left join workMatch on CleverReports_middleInput.ReleaseDate = workMatch.calendar_date;
 END
