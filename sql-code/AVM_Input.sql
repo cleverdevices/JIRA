e4cdaeab0c4fb76af5fee07c6b12740e61cce045
cd-ktreanor <cd.ktreanor@gmail.com>
2017-07-13 10:58:19 -0400

    Update AVM_Input.sql

--- sql-code/AVM_Input.sql
+++ sql-code/AVM_Input.sql
@@ -7,11 +7,13 @@ create table AVM_InputTable
     
     Select AVM_middleInput.* ,
 
-ifnull(case when AVM_middleInput.Ticket_Closed = 1 then 0 else (case when (AVM_middleInput.Closed_Story_Points + AVM_middleInput.Remaining_Story_Points = 0) then AVM_middleInput.Proposal_or_NRE_Hours else (case when (AVM_middleInput.Closed_Story_Points + AVM_middleInput.Remaining_Story_Points) > AVM_middleInput.Proposal_or_NRE_Hours then AVM_middleInput.Remaining_Story_Points else (AVM_middleInput.Proposal_or_NRE_Hours - AVM_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use
+ifnull(case when AVM_middleInput.Ticket_Closed = 1 then 0 else (case when (AVM_middleInput.Closed_Story_Points + AVM_middleInput.Remaining_Story_Points = 0) then AVM_middleInput.Proposal_or_NRE_Hours else (case when (AVM_middleInput.Closed_Story_Points + AVM_middleInput.Remaining_Story_Points) > AVM_middleInput.Proposal_or_NRE_Hours then AVM_middleInput.Remaining_Story_Points else (AVM_middleInput.Proposal_or_NRE_Hours - AVM_middleInput.Closed_Story_Points) end) end) end, 0) as Hours_to_Use,
+workMatch.releaseRow as Release_Actual
 
-from AVM_middleInput ;
 
-select AVM_InputTable.*, workMatch.releaseRow as Release_Actual from AVM_InputTable
-left join workMatch on AVM_InputTable.ReleaseDate = workMatch.calendar_date;
+from AVM_middleInput 
+left join workMatch on AVM_middleInput.ReleaseDate = workMatch.calendar_date;
+
+
 
 END
